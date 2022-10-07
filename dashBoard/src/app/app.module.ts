dcbe828c9501d0df58f0a0618dceccb14aabdd06
Rajesh <Raaj.Everest@gmail.com>
2022-10-08 00:29:49 +0530

    DashBoard component created

--- dashBoard/src/app/app.module.ts
+++ dashBoard/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDashBoardComponent } from './user-dash-board/user-dash-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDashBoardComponent
   ],
   imports: [
     BrowserModule,
