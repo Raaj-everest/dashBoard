1e6436f9fcd320c48bdd552e753c90dfaf5327b1
Rajesh <Raaj.Everest@gmail.com>
2022-10-08 00:57:14 +0530

    Reactive form created in user dashBoard

--- dashBoard/src/app/user-dash-board/user-dash-board.component.ts
+++ dashBoard/src/app/user-dash-board/user-dash-board.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormBuilder,FormControl} from '@angular/forms'
+import { User } from '../Interfaces/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-dash-board',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashBoardComponent implements OnInit {
 
-  constructor() { }
+  private user!: User;
+  userForm!: FormGroup;
+  name!:String;
+  emailId!:string;
+  avatar="https://pdtxar.com/wp-content/uploads/2019/04/person-placeholder.jpg";
+
+  constructor(private userService:UserService,private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+this.userForm = this.formBuilder.group({
+  emailId:[""],
+  name:[""],
+  avatar :["https://pdtxar.com/wp-content/uploads/2019/04/person-placeholder.jpg"]
+})
   }
-
+ getUser(id:number){
+      this.userService.getUsers(id).subscribe(response=>{
+        this.user=response;
+      });
+      this.emailId=this.user.data.email;
+      this.avatar = this.user.data.avatar;
+      this.name = (this.user.data.first_name+" "+this.user.data.last_name)
+ }
 }
