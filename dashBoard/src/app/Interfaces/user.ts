116fb789f08fcbea133452da73f3ad73c9c35c6b
Rajesh <Raaj.Everest@gmail.com>
2022-10-08 00:37:12 +0530

    User Interface created

---
+++ dashBoard/src/app/Interfaces/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    data:
+    {
+        id: number,
+        email: string,
+        first_name: string,
+        last_name: string,
+        avatar: string
+    },
+    support:
+    { url: string, text: string }
+}
